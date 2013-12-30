955aa5110ab6dcd9227abbfe828add143b81ad46
farukunal <faukunal@gmail.com>
2013-12-31 01:39:35 +0200

    SagSol kisi msjları gorsel
    mesaj görselleri geliştirilmiş

---
+++ temp/src/com/example/babap/AwesomeGorsel.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
+
+public class AwesomeAdapter extends BaseAdapter{
+	private Context mContext;
+	private ArrayList<Message> mMessages;
+
+	public AwesomeAdapter(Context context, ArrayList<Message> messages) {
+		super();
+		this.mContext = context;
+		this.mMessages = messages;
+	}
+	@Override
+	public int getCount() {
+		return mMessages.size();
+	}
+	@Override
+	public Object getItem(int position) {		
+		return mMessages.get(position);
+	}
+	
+
+}
