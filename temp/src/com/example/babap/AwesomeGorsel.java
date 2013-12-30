77d207a9c0082033e8cc2716e14b8cc8eeffbb50
faukunal@gmail.com <fk20111206git>
2013-12-31 01:46:52 +0200

    msj gorsel geliştirildi
    View gorsel bağlantılar tamam

--- temp/src/com/example/babap/AwesomeGorsel.java
+++ temp/src/com/example/babap/AwesomeGorsel.java
@@ -28,5 +28,47 @@ public class AwesomeAdapter extends BaseAdapter{
 		return mMessages.get(position);
 	}
 	
+	
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		Message message = (Message) this.getItem(position);
+
+		ViewHolder holder; 
+		if(convertView == null)
+		{
+			holder = new ViewHolder();
+			convertView = LayoutInflater.from(mContext).inflate(R.layout.sms_row, parent, false);
+			holder.message = (TextView) convertView.findViewById(R.id.message_text);
+			convertView.setTag(holder);
+		}
+		else
+			holder = (ViewHolder) convertView.getTag();
+		
+		holder.message.setText(message.getMessage());
+		
+		LayoutParams lp = (LayoutParams) holder.message.getLayoutParams();
+		if(message.isStatusMessage())
+		{
+			holder.message.setBackgroundDrawable(null);
+			lp.gravity = Gravity.LEFT;
+			holder.message.setTextColor(R.color.textFieldColor);
+		}
+		else
+		{		
+			if(message.isMine())
+			{
+				holder.message.setBackgroundResource(R.drawable.speech_bubble_green);
+				lp.gravity = Gravity.RIGHT;
+			}
+			else
+			{
+				holder.message.setBackgroundResource(R.drawable.speech_bubble_orange);
+				lp.gravity = Gravity.LEFT;
+			}
+			holder.message.setLayoutParams(lp);
+			holder.message.setTextColor(R.color.textColor);	
+		}
+		return convertView;
+	}
 
 }
